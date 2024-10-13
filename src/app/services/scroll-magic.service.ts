5fc75fbd2c814d35483c45f37162119a74dcc7d6
vinoditprobs <itprobs.com@gamail.com>
2024-10-13 12:21:01 +0530

    done

--- src/app/services/scroll-magic.service.ts
+++ src/app/services/scroll-magic.service.ts
@@ -50,7 +50,7 @@ export class ScrollMagicService {
           triggerHook: 1,
           duration: '100%'
         })
-        .setTween(element.querySelector('.parallax_bg .video_player video'), {y: "100%"})
+        .setTween(element.querySelector('.parallax_bg .video_player video'), {y: "100%",  ease:'liner' })
         //.addIndicators({ name: `Parallax ${index + 1}` })
         .addTo(controller);
       });
