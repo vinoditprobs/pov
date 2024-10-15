2979084079dd9e9fd94c161c41f01ab2632ac3b0
vinoditprobs <itprobs.com@gamail.com>
2024-10-15 15:30:49 +0530

    done

--- src/app/shared/slide-show/slide-show.component.ts
+++ src/app/shared/slide-show/slide-show.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject, PLATFORM_ID, Renderer2, ElementRef, Input } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, AfterViewInit, Input } from '@angular/core';
 import { isPlatformBrowser, DOCUMENT } from '@angular/common';
 import Swiper from 'swiper';
 
@@ -7,7 +7,7 @@ import Swiper from 'swiper';
   templateUrl: './slide-show.component.html',
   styleUrl: './slide-show.component.scss'
 })
-export class SlideShowComponent {
+export class SlideShowComponent implements AfterViewInit {
 
   @Input() sliderSlides: any[] = [];
   @Input() sliderPagination: boolean = true;
@@ -21,31 +21,34 @@ export class SlideShowComponent {
      @Inject(PLATFORM_ID) private platformId: Object,
      @Inject(DOCUMENT) private document: Document,) { }
  
+
+     private initSwiper() {
+      if (isPlatformBrowser(this.platformId)) {
+        this.swiper = new Swiper(".slideShow", {
+          slidesPerView: 1,
+          spaceBetween: 0,
+          loop:true,
+          centeredSlides:true,
+         //autoHeight: false,
+          //effect: "fade",
+         // direction:'vertical',
+          navigation: false,
+          pagination: false,
+          allowTouchMove: false,
+          speed: 2000,
+          autoplay: {
+            delay: 4000,
+            disableOnInteraction: true,
+          }
+          
+        });
+
+  
+      }
+  }    
  
- ngOnInit():void{
- 
-   if (isPlatformBrowser(this.platformId)) {
- 
-     this.swiper = new Swiper(".slideShow", {
-       slidesPerView: 1,
-       spaceBetween: 0,
-       loop:true,
-       centeredSlides:true,
-      //autoHeight: false,
-       //effect: "fade",
-      // direction:'vertical',
-       navigation: false,
-       pagination: false,
-       allowTouchMove: false,
-       speed: 2000,
-       autoplay: {
-         delay: 4000,
-         disableOnInteraction: true,
-       }
-       
-     });
- 
-   }
+ ngAfterViewInit():void{
+  this.initSwiper();
  }
  
 }
