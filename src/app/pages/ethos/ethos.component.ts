29428ba8575984e0e0801aef4eaeac885331fb13
vinoditprobs <itprobs.com@gamail.com>
2024-10-21 13:27:18 +0530

    done

--- src/app/pages/ethos/ethos.component.ts
+++ src/app/pages/ethos/ethos.component.ts
@@ -9,6 +9,6 @@ import { NavbarThemeService } from '../../services/navbar-theme.service';
 export class EthosComponent {
   constructor(private NavbarThemeService: NavbarThemeService){}
   ngOnInit(){
-    this.NavbarThemeService.changeHeaderTheme('light')
+    this.NavbarThemeService.changeHeaderTheme('dark')
   }
 }
