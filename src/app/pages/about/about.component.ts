1f1d1ad3073e19bd4cf3e516f8367072e3639330
vinoditprobs <itprobs.com@gamail.com>
2024-10-22 12:49:31 +0530

    done

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -59,4 +59,56 @@ export class AboutComponent {
 
   }
   
+
+  clients = [
+    {
+      img:'images/clients/skechers.png',
+      title:'Skechers'
+    },
+    {
+      img:'images/clients/zee-digital.png',
+      title:'Zee Digital'
+    },
+    {
+      img:'images/clients/nahar.png',
+      title:'Nahar'
+    },
+    {
+      img:'images/clients/kaveri.png',
+      title:'Kaveri'
+    },
+    {
+      img:'images/clients/icici.png',
+      title:'ICICI Prudential'
+    },
+    {
+      img:'images/clients/garden-vareli.png',
+      title:'Garden Vareli'
+    },
+    {
+      img:'images/clients/essence.png',
+      title:'Essence'
+    },
+    {
+      img:'images/clients/baggit.png',
+      title:'Baggit'
+    },
+    {
+      img:'images/clients/aditya-birla-group.png',
+      title:'Aditya Birla Group'
+    },
+    {
+      img:'images/clients/360-one.png',
+      title:'360 One'
+    },
+    {
+      img:'images/clients/shemaroo.png',
+      title:'Shemaroo'
+    },
+    {
+      img:'images/clients/aditya-birla-group.png',
+      title:'Mini Plex'
+    },
+  ]
+
 }
