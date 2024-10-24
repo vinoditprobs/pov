b27acbfce5704b552125f453b61ed8d2b3776133
vinoditprobs <itprobs.com@gamail.com>
2024-10-24 16:21:50 +0530

    done

--- src/app/pages/work/work.component.ts
+++ src/app/pages/work/work.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavbarThemeService } from '../../services/navbar-theme.service';
 import { ScrollMagicService } from '../../services/scroll-magic.service';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-work',
@@ -9,10 +10,19 @@ import { ScrollMagicService } from '../../services/scroll-magic.service';
 })
 export class WorkComponent {
 
-  constructor(private NavbarThemeService: NavbarThemeService, private ScrollMagicService: ScrollMagicService){}
+  projects: any = []
+
+  constructor(private NavbarThemeService: NavbarThemeService, private ScrollMagicService: ScrollMagicService, private DataService: DataService){}
   ngOnInit(){
     this.NavbarThemeService.changeHeaderTheme('dark');
     this.ScrollMagicService.initScrollMagic();
+
+    this.DataService.getProjects().subscribe(data => {
+
+      this.projects = data
+
+    })
+
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -10,12 +10,19 @@ import { Observable, catchError, of } from 'rxjs';
 export class DataService {
 
   private clients = 'data/clients.json'
+  private projects = 'data/projects.json'
 
   constructor(private http: HttpClient) { }
 
   getClients(): Observable<any[]>{
     return this.http.get<any[]>(this.clients).pipe(
-      catchError(this.handleError<any[]>('getProjectsData', []))
+      catchError(this.handleError<any[]>('getClients', []))
+    );
+  }
+
+  getProjects(): Observable<any[]>{
+    return this.http.get<any[]>(this.projects).pipe(
+      catchError(this.handleError<any[]>('getProjects', []))
     );
   }
 
