57fccd93265af226878cd4c70ad8140bc736673a
vinoditprobs <itprobs.com@gamail.com>
2024-10-25 12:14:05 +0530

    revert

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,53 +6,12 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { WorkComponent } from './pages/work/work.component';
 import { EthosComponent } from './pages/ethos/ethos.component';
 
-import { SkechersComponent } from './pages/projects/skechers/skechers.component';
-import { EssenceComponent } from './pages/projects/essence/essence.component';
-import { KaveriComponent } from './pages/projects/kaveri/kaveri.component';
-import { ZoiComponent } from './pages/projects/zoi/zoi.component';
-import { BaggitComponent } from './pages/projects/baggit/baggit.component';
-import { OzeeComponent } from './pages/projects/ozee/ozee.component';
-import { EverEnviroComponent } from './pages/projects/ever-enviro/ever-enviro.component';
-import { One360Component } from './pages/projects/one-360/one-360.component';
-import { CccComponent } from './pages/projects/ccc/ccc.component';
-
-
 const routes: Routes = [
   { path:'', component:HomeComponent },
   { path:'about', component:AboutComponent },
   { path:'contact', component:ContactComponent},
   { path:'ethos', component:EthosComponent},
-  { path:'work', component:WorkComponent,
-    children:[
-      {
-        path:'skechers', component:SkechersComponent,
-      },
-      {
-        path:'essence', component:EssenceComponent,
-      },
-      {
-        path:'kaveri', component:KaveriComponent,
-      },
-      {
-        path:'zoi', component:ZoiComponent,
-      },
-      {
-        path:'baggit', component:BaggitComponent,
-      },
-      {
-        path:'ozee', component:OzeeComponent,
-      },
-      {
-        path:'ever-enviro', component:EverEnviroComponent,
-      },
-      {
-        path:'one-360', component:One360Component,
-      },
-      {
-        path:'ccc', component:CccComponent,
-      },
-    ]
-  },
+  { path:'work', component:WorkComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,15 +24,7 @@ import { ParallaxKeywordComponent } from './components/parallax-keyword/parallax
 import { InteractiveCursorSectionComponent } from './components/interactive-cursor-section/interactive-cursor-section.component';
 import { CursorComponent } from './shared/cursor/cursor.component';
 import { KeyFeaturesComponent } from './components/key-features/key-features.component';
-import { SkechersComponent } from './pages/projects/skechers/skechers.component';
-import { EssenceComponent } from './pages/projects/essence/essence.component';
-import { KaveriComponent } from './pages/projects/kaveri/kaveri.component';
-import { ZoiComponent } from './pages/projects/zoi/zoi.component';
-import { BaggitComponent } from './pages/projects/baggit/baggit.component';
-import { OzeeComponent } from './pages/projects/ozee/ozee.component';
-import { EverEnviroComponent } from './pages/projects/ever-enviro/ever-enviro.component';
-import { One360Component } from './pages/projects/one-360/one-360.component';
-import { CccComponent } from './pages/projects/ccc/ccc.component';
+
 
 @NgModule({
   declarations: [
@@ -51,16 +43,7 @@ import { CccComponent } from './pages/projects/ccc/ccc.component';
     ParallaxKeywordComponent,
     InteractiveCursorSectionComponent,
     CursorComponent,
-    KeyFeaturesComponent,
-    SkechersComponent,
-    EssenceComponent,
-    KaveriComponent,
-    ZoiComponent,
-    BaggitComponent,
-    OzeeComponent,
-    EverEnviroComponent,
-    One360Component,
-    CccComponent
+    KeyFeaturesComponent
   ],
   imports: [
     BrowserModule,
